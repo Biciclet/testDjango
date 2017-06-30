7e206da67309844584e7598bb416df8638b2c9b2
ange <ange@localhost.localdomain>
2017-06-30 09:57:42 +0200

    ajout de nouvelles pages et fonctionnalités blog date et addition

---
+++ blog/views.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, redirect
+
+from django.http import HttpResponse, Http404
+
+from datetime import datetime
+
+# Create your views here.
+
+def home(request):
+    """ Exemple de page HTML, non valide pour que l'exemple soit concis """
+    text = """<h1>Bienvenue sur mon blog !</h1>
+              <p>Les crêpes bretonnes ça tue des mouettes en plein vol !</p>"""
+    return HttpResponse(text)
+
+def list_articles(request, month, year):
+    """ Liste des articles d'un mois précis. """
+    return HttpResponse(
+        "Vous avez demandé les articles de {0} {1}.".format(month, year)
+    )
+
+
+def view_article(request, id_article):
+    # Si l'ID est supérieur à 100, nous considérons que l'article n'existe pas
+    if int(id_article) > 100:
+        raise Http404
+
+    return HttpResponse('<h1>Mon article ici</h1>')
+
+
+def view_article(request, id_article):
+    if int(id_article) > 100:
+        raise Http404
+
+    return redirect(view_redirection)
+
+def view_redirection(request):
+    return HttpResponse("Vous avez été redirigé.")
+
+
+def date_actuelle(request):
+
+    return render(request, 'blog/date.html', {'date': datetime.now()})
+
+
+
+def addition(request, nombre1, nombre2):
+
+    total = int(nombre1) + int(nombre2)
+
+
+    # Retourne nombre1, nombre2 et la somme des deux au tpl
+
+    return render(request, 'blog/addition.html', locals())
